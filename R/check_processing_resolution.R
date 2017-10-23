a68858a66bd78f59f2dcd32009e158565bf86a6a
Mirza Cengic <mirzaceng@gmail.com>
2017-10-23 11:33:21 +0200

    Added function documentation

--- R/check_processing_resolution.R
+++ R/check_processing_resolution.R
@@ -1,3 +1,14 @@
+#' Check processing resolution of species.
+#'
+#' Checks with which processing resolution the species has 30 or more points.
+#'
+#' @param in_species Shapefile of the species being checked for.
+#'
+#' @return A string with the processing resolution
+#' @export
+#'
+#' @examples
+#'
 check_processing_resolution <- function(in_species)
 {
 proc_resolution <- "10m"
@@ -5,48 +16,48 @@ proc_resolution <- "10m"
 raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
                            pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
 raster_mask <- raster(raster_files[1])
-						   
+
 species_range <- fasterize(in_species, raster_mask)
 species_range_points <- rasterToPoints(species_range)
 
 if (nrow(species_range_points) <= 30) {
     cat(paste0("Less than 30 points for resolution ", proc_resolution), "\n")
-    
+
 	proc_resolution <- "5m"
-	
+
 	raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
                            pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
     raster_mask <- raster(raster_files[1])
-			
-	
+
+
 	species_range <- fasterize(in_species, raster_mask)
     species_range_points <- rasterToPoints(species_range)
 	}
-	
+
 if (nrow(species_range_points) <= 30) {
     cat(paste0("Less than 30 points for resolution ", proc_resolution), "\n")
-    
+
 	proc_resolution <- "2.5m"
-	
+
 	raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
                            pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
     raster_mask <- raster(raster_files[1])
-			
-	
+
+
 	species_range <- fasterize(in_species, raster_mask)
     species_range_points <- rasterToPoints(species_range)
 	}
-	
+
 if (nrow(species_range_points) <= 30) {
     cat(paste0("Less than 30 points for resolution ", proc_resolution), "\n")
-    
+
 	proc_resolution <- "30s"
-	
+
 	raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
                            pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
     raster_mask <- raster(raster_files[1])
-			
-	
+
+
 	species_range <- fasterize(in_species, raster_mask)
     species_range_points <- rasterToPoints(species_range)
 	}
@@ -74,5 +85,5 @@ file.create(outfile)
 }
 
 
-return(proc_resolution)	
+return(proc_resolution)
 }
