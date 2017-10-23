5c00ff4e0320a52c72506f68cb12cb1f81dea865
Mirza Cengic <mirzaceng@gmail.com>
2017-10-23 11:23:33 +0200

    Added first version of check processing resolution script that checks the resolution on which there are more than 30 species points for modeling

---
+++ R/check_processing_resolution.R
@@ -0,0 +1,78 @@
+check_processing_resolution <- function(in_species)
+{
+proc_resolution <- "10m"
+
+raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
+                           pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
+raster_mask <- raster(raster_files[1])
+						   
+species_range <- fasterize(in_species, raster_mask)
+species_range_points <- rasterToPoints(species_range)
+
+if (nrow(species_range_points) <= 30) {
+    cat(paste0("Less than 30 points for resolution ", proc_resolution), "\n")
+    
+	proc_resolution <- "5m"
+	
+	raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
+                           pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
+    raster_mask <- raster(raster_files[1])
+			
+	
+	species_range <- fasterize(in_species, raster_mask)
+    species_range_points <- rasterToPoints(species_range)
+	}
+	
+if (nrow(species_range_points) <= 30) {
+    cat(paste0("Less than 30 points for resolution ", proc_resolution), "\n")
+    
+	proc_resolution <- "2.5m"
+	
+	raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
+                           pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
+    raster_mask <- raster(raster_files[1])
+			
+	
+	species_range <- fasterize(in_species, raster_mask)
+    species_range_points <- rasterToPoints(species_range)
+	}
+	
+if (nrow(species_range_points) <= 30) {
+    cat(paste0("Less than 30 points for resolution ", proc_resolution), "\n")
+    
+	proc_resolution <- "30s"
+	
+	raster_files <- list.files(paste0(.basepath_milkun1, "Data_RAW/WorldClim/bioclim"),
+                           pattern = paste0(proc_resolution, ".*.tif"), full.names = TRUE, recursive = TRUE)
+    raster_mask <- raster(raster_files[1])
+			
+	
+	species_range <- fasterize(in_species, raster_mask)
+    species_range_points <- rasterToPoints(species_range)
+	}
+
+if (nrow(species_range_points) <= 30)
+{
+proc_resolution <- "Insufficient"
+}
+
+
+outdir <- paste0(milkunize("Projects/Amphibians/R/Output/Modeling_resolution/"), proc_resolution)
+
+if (!dir.exists(outdir))
+{
+  dir.create(outdir)
+  message("Output directory created")
+}
+
+outfile <- paste0(outdir, "/", unique(in_species$binomial))
+
+if (!dir.exists(outfile))
+{
+file.create(outfile)
+  message("Output file created")
+}
+
+
+return(proc_resolution)	
+}
